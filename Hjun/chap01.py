b20e550a095cd283f2645490c1f29f8ce8bd8d7a
leeceo97 <leeceo97@naver.com>
2021-06-29 12:44:36 +0900

    chap1 완료

---
+++ Hjun/chap01.py
@@ -0,0 +1,33 @@
+# 001
+print("Hello World")
+
+# 002
+print('Mary\'s cosmetics')
+
+#003
+print("신씨가 소리질렀다. \"도둑이야\".")
+
+#004
+print("\"C:\\Windows\"")
+
+#005
+print("\"안녕하세요.\n만나서\t\t반갑습니다.")
+# \n은 띄어쓰기, \t는 탭
+
+#006
+# 오늘은 일요일과 같이 옆으로 출력이된다.
+
+#007
+print("naver\;kakao\;sk\;samsung")
+# 해설지 풀이
+print('naver', 'kakao', 'sk', 'samsung', sep=';')
+# sep은 각각 따로 선언된 값들 사이에 선정한 문자를 끼워넣어 출력하는것
+
+#008
+print('naver','kakao','sk','samsung',sep='/')
+
+#009
+print("first", end=""); print("second")
+
+#010
+print(5/3)
