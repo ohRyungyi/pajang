ded67e8007500a539f9969054f4e98fc3445055d
ssooda <84752537+ssooda@users.noreply.github.com>
2021-06-29 16:01:20 +0900

    Add files via upload

---
+++ to100.py
@@ -0,0 +1,39 @@
+
+# 1
+print('Hello World')
+
+# 2
+print("Mary's cosmetics")
+print('Mars\'s cosmetics')
+
+# 3
+print('신씨가 소리질렀다. "도둑이야".')
+
+# 4
+print('C:\Windows')
+
+# 5
+print("안녕하세요.\n 만나서\t\t반갑습니다.")
+# \n : 줄바꿈
+# \t : tap 키
+
+# 6
+print("오늘은", "일요일")
+# ,와 +의 차이점
+
+# 7
+print("naver", "kakao", "samsung", sep=";")
+# ,와 sep =" "
+
+# 8
+print('naver', 'kakao', 'sk', 'samsung', sep="/")
+
+# 9
+print('first', end="\n")
+print('second')
+# sep과 end의 차이
+# sep을 사용하지 않으면 sep=" "와 동일
+# end를 사용하지 않으면 end="\n"과 동일
+
+# 10
+print(5/3)
