c5de42a56184dfd86d8df9c87b0f3eb0296ed75a
leeceo97 <leeceo97@naver.com>
2021-07-01 02:56:19 +0900

    chap07-2

---
+++ Hjun/chap07-2.py
@@ -0,0 +1,72 @@
+# 111
+#c = input()
+#print(c*2)
+
+# 112
+#c = int(input("숫자를 입력하세요: "))
+#print(c+10)
+
+# 113
+#c = int(input())
+#if c % 2==0:
+#    print('짝수')
+#else:
+#    print('홀수')
+
+# 114
+#c = int(input())
+#d = c+20
+#if d >255:
+#    print(255)
+#else:
+#    print(d)
+
+# 115
+#c = int(input())
+#d = c-20
+#if d<0:
+#    print(0)
+#elif d>255:
+#    print(255)
+#else:
+#    print(d)
+
+# 116
+#a,b = input().split(':')
+#if b!='00':
+#    print('정각이 아닙니다.')
+#else:
+#    print('정각입니다.')
+
+# 117
+#fruit = ["사과", "포도", "홍시"]
+#c = input('좋아하는 과일은? ')
+#if c in fruit:
+#    print('정답입니다.')
+#else:
+#    print('오답입니다.')
+
+# 118
+#warn_investment_list = ["Microsoft", "Google", "Naver", "Kakao", "SAMSUNG", "LG"]
+#c = input()
+#if c in warn_investment_list:
+#    print('투자경고 종목입니다.')
+#else:
+#    print('투자 경고 종목이 아닙니다.')
+
+# 119
+fruit = {"봄" : "딸기", "여름" : "토마토", "가을" : "사과"}
+c = input()
+#if c in fruit.keys():
+#    print('정답입니다.')
+#else:
+#    print('오답입니다.')
+
+# 120
+if c in fruit.values():
+    print('정답니다.')
+else:
+    print('오답입니다.')
+
+# 후기
+# 분기문 파트는 쉬어가는 파트인게 확실하다...
