f63580f15d35d809ae48ac87ade62cf2a21e56fb
leeceo97 <leeceo97@naver.com>
2021-07-01 02:32:07 +0900

    chap06-2

---
+++ Hjun/chap06-2.py
@@ -0,0 +1,53 @@
+# 091
+ice = {
+    '메로나':[300,20],
+    '비비빅':[400,3],
+    '죠스바':[250,100]
+}
+print(ice)
+
+# 092
+print('%d 원'%ice['메로나'][0])
+
+# 093
+print('%d 개'%ice['메로나'][1])
+
+# 094
+ice['월드콘'] = [500,7]
+print(ice)
+
+# 095
+icecream = {
+    '탱크보이': 1200,
+    '폴라포': 1200,
+    '빵빠레': 1800,
+    '월드콘': 1500,
+    '메로나': 1000
+}
+icecream_keys = list(icecream.keys())
+print(icecream_keys)
+
+# 096
+icecream_values = list(icecream.values())
+print(icecream_values)
+
+# 097
+icecream_values_sum = sum(icecream_values)
+print(icecream_values_sum)
+
+# 098
+new_product = {'팥빙수':2700, '아맛나':1000}
+icecream.update(new_product)
+print(icecream)
+
+# 099
+keys = ("apple", "pear", "peach")
+vals = (300, 250, 400)
+result = dict(zip(keys,vals))
+print(result)
+
+# 100
+date = ['09/05', '09/06', '09/07', '09/08', '09/09']
+close_price = [10500, 10300, 10100, 10800, 11000]
+close_table = dict(zip(date,close_price))
+print(close_table)
