d028411eb061c454beaabd4690c7d635f47f1ac7
ohryungyi <dodoh0125@konkuk.ac.kr>
2021-07-03 16:19:59 +0900

    1주차 목표: 150번까지 풀이 complete

--- 5ryung2/for_while.py
+++ 5ryung2/for_while.py
@@ -187,8 +187,10 @@
 #     if file.split('.')[-1] == 'h':
 #         print(file)
 
-# 159 -
-리스트 = ['intra.h', 'intra.c', 'define.h', 'run.py']
-for file in 리스트:
-    if file.split('.')[-1] == "h" or file.split('.')[-1] == "c":
-        print(file)
+# # 159 -
+# 리스트 = ['intra.h', 'intra.c', 'define.h', 'run.py']
+# for file in 리스트:
+#     if file.split('.')[-1] == "h" or file.split('.')[-1] == "c":
+#         print(file)
+
+# 160 - 
