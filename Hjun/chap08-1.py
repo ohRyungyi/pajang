6730dc45c610354033ab87ae8a331e86da0ffaf0
leeceo97 <leeceo97@naver.com>
2021-07-03 15:44:48 +0900

    chap8-1

---
+++ Hjun/chap08-1.py
@@ -0,0 +1,53 @@
+# 131
+# 사과
+# 귤
+# 수박
+
+# 132
+# #####
+# #####
+# #####
+
+# 133
+print("A","B","C",sep="\n")
+
+# 134
+print("출력:\"A\"")
+print("출력:\"B\"")
+print("출력:\"C\"")
+
+# 135
+str_a = "A"
+str_b = "B"
+str_c = "C"
+print(f"변환: {str_a.lower()}")
+print(f"변환: {str_b.lower()}")
+print(f"변환: {str_c.lower()}")
+
+# 136
+변수 = [10,20,30]
+for i in 변수:
+    print(i)
+
+# 137
+변수 = [10,20,30]
+for i in 변수:
+    print(i)
+
+# 138
+for i in 변수:
+    print(i)
+    print("-------")
+
+# 139
+print('++++')
+for i in 변수:
+    print(i)
+
+# 140
+for i in range(4):
+    print("--------")
+
+# 후기
+# 문제 자체는 많지 않았으나 매일매일 학습할 분량을
+# 정해서 하는게 좋을것 같다는 생각이 들어 하루 30문제를 풀생각해야겠다.
