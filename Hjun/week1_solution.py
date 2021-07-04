46d10d29ed8a6386e58f2ed2ed9138a8a8c220d5
leeceo97 <leeceo97@naver.com>
2021-07-04 21:28:43 +0900

    week1_solution

---
+++ Hjun/week1_solution.py
@@ -0,0 +1,29 @@
+# 코딩테스트 대회가 개최되었습니다. 이 때, 참여자들 이름을 공백을 통해 구분을 하여 입력을 합니다.
+# 예) "홍길동" "김길동" "최길동"
+# 그 후 코딩테스트에서 순차대로 각 대회에서 맞춘 문제를 공백을 통해 구분하여 입력이 됩니다.
+# 예) 2 10 4
+# 만약 중간에 기권을 했을 경우 '기권'이라고 입력이 됩니다.
+# 예) 2 10 기권
+# 이 때 기권을 하지 않은 학생들의 성적의 평균을 넘긴 학생들만은 참가상을 모두 받을 수 있습니다.
+# 참가상을 받은 학생들을 푼 점수가 높은 순서대로 공백으로 구분하여 출력해 주세요
+import operator
+names = input().split(' ')
+scores = input().split(' ')
+dictionary = dict(zip(names,scores))
+sum = 0
+awards = []
+for i in dictionary.keys():
+    if "기권" in dictionary[i]:
+        del dictionary[i]
+    else:
+        sum += int(dictionary[i])
+avg = int(sum/len(dictionary.keys()))
+for i in dictionary.keys():
+    dictionary[i] = int(dictionary[i])
+    if dictionary[i] < avg:
+        del dictionary[i]
+    else:
+        continue
+
+sdict= sorted(dictionary.items(), key=operator.itemgetter(0))
+print(dictionary.keys())
--- Hjun/week1_test.py
+++ Hjun/week1_test.py
@@ -3,3 +3,36 @@
 # 각각 입력받은 나이, 이름 이 두입력값들을 합쳐 딕셔너리 형태로 만들고
 # for문을 사용하여 각자의 나이에 3년을 더한값으로 업데이트 후 이름과 나이를 출력하시오.
 # 출력 예(철수:24, 영희:25, 형준:29 ...)
+
+friends = input().split(' ')
+age = input().split(' ')
+age_int = []
+for i in age:
+    age_int.append(int(i)+3)
+
+dictionary = dict(zip(friends,age_int))
+print(', '.join('{}:{}'.format(k,v) for k,v in dictionary.items()))
+
+# 사용자에게 5자리 숫자를 입력받는다
+# "일의 자리"부터 각 자리수에 3,4,5,6,7을 곱한 뒤 그 값을 전부 더한다 (#합계의 변수는 sum으로, #for문을 사용하세요)
+# sum을 3으로 나눈 나머지를 고유번호로 알려준다 (#format을 사용하세요)
+# ** 각 과정이 보일 수 있도록 for 문을 작성하세요
+
+# ex. 
+# 5자리 숫자를 입력하세요 : 12345
+# 5 x 3 = 15
+# 4 x 4 = 16
+# 3 x 5 = 15
+# 2 x 6 = 12
+# 1 x 7 = 7
+# 합계 : 65
+# 고유번호는 "2" 입니다. (#반드시  "고유번호"로 나타내주세요)
+
+user_input = input()
+sum = 0
+key = 3
+for i in user_input[::-1]:
+    sum+= int(i)*key
+    key+=1
+result = sum%3
+print(f'고유번호는 \"{result}\" 입니다.')
