1d3243806c102d270cfc4147b7671159e1647a76
leeceo97 <leeceo97@naver.com>
2021-07-06 22:48:15 +0900

    chap09-1

---
+++ Hjun/chap09-1.py
@@ -0,0 +1,36 @@
+# 201
+def print_coin():
+    print('비트코인')
+
+# 202
+print_coin()
+
+# 203
+for i in range(1,101):
+    print_coin()
+
+# 204
+def print_coin():
+    for i in range(100):
+        print('비트코인')
+
+# 205
+# 함수가 선언되기 전에 호출을해서
+
+# 206
+# A
+# B
+# C
+# A
+# B
+
+# 207
+# A
+# C
+# B
+
+# 208
+# A
+# C
+# B
+# D
